33d6a8a2c9d59bd731e490973b7bc9f984ad5f25
fifsun <84824248+fifsun@users.noreply.github.com>
2022-12-19 14:59:14 +0900

    API追加

---
+++ app/src/main/java/dj_s5_EW6AX_EL/CouplingSolenoidDutyCycle.kt
@@ -0,0 +1,17 @@
+package dj_s5_EW6AX_EL
+
+import com.github.eltonvs.obd.command.ObdCommand
+import com.github.eltonvs.obd.command.ObdRawResponse
+import com.github.eltonvs.obd.command.bytesToInt
+
+class CouplingSolenoidDutyCycle : ObdCommand() {
+        // Required
+        override val tag = "AWD_CSDC_RATIO"
+        override val name = "Coupling Solenoid Duty Cycle"
+        override val mode = "01"
+        override val pid = "221E7A"
+
+        // Optional
+        override val defaultUnit = "%"
+        override val handler = { it: ObdRawResponse ->"%.1f".format(bytesToInt(it.bufferedValue)) }
+}
---
+++ app/src/main/java/dj_s5_aie/DPFRegenerationStatus.kt
@@ -0,0 +1,25 @@
+package dj_s5_aie
+
+import com.github.eltonvs.obd.command.ObdCommand
+import com.github.eltonvs.obd.command.ObdRawResponse
+import com.github.eltonvs.obd.command.ObdResponse
+
+class DPFRegenerationStatus : ObdCommand() {
+        // Required
+        override val tag = "DPF_REG_STS"
+        override val name = "DPF Regeneration Status"
+        override val mode = "01"
+        override val pid = "220380"
+
+        // Optional
+        override val handler = { it: ObdRawResponse ->
+                val dpf = it.bufferedValue[2]
+                val dpfSts = (dpf and 0x80) == 128
+                dpfSts.toString()
+        }
+
+        override fun format(response: ObdResponse): String {
+                val dpfSts = response.value.toBoolean()
+                return "DPF_REG_STS is ${if (dpfSts) "ON" else "OFF"}"
+        }
+}
