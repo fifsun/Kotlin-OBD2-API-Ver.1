043e961ee60240fcd9f831c626b65d4e8ffcfa93
fifsun <84824248+fifsun@users.noreply.github.com>
2023-02-06 15:00:33 +0900

    ファイル構造変更・API追加

---
+++ app/src/main/java/com/example/kotlin_obd2_apiver1/ui/adapterSetup/AdapterSearchActivity.kt
@@ -0,0 +1,18 @@
+package com.example.kotlin_obd2_apiver1.ui.adapterSetup
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlin_obd2_apiver1.R
+
+class AdapterSearchActivity : AppCompatActivity() {
+
+    @Suppress("DEPRECATION")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_adapter_setup)
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/kotlin_obd2_apiver1/ui/adapterSetup/AdapterSetupActivity.kt
@@ -0,0 +1,23 @@
+package com.example.kotlin_obd2_apiver1.ui.adapterSetup
+
+import android.R
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class AdapterSetupActivity : AppCompatActivity() {
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(com.example.kotlin_obd2_apiver1.R.layout.activity_adapter_setup)
+
+        }
+
+}
